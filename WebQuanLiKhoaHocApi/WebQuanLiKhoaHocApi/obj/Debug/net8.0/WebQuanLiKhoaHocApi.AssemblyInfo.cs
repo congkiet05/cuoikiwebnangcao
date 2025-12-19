06e68d6b15a0f40bcd42c46851d47027a155ca2e
congkiet05 <congkiet05@gmail.com>
2025-12-19 22:25:53 +0700

    còn thiếu nhắn tin thời gian thực

--- WebQuanLiKhoaHocApi/WebQuanLiKhoaHocApi/obj/Debug/net8.0/WebQuanLiKhoaHocApi.AssemblyInfo.cs
+++ WebQuanLiKhoaHocApi/WebQuanLiKhoaHocApi/obj/Debug/net8.0/WebQuanLiKhoaHocApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebQuanLiKhoaHocApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be6d106ecff7fec092ab18e1fdfd0ca22dc5e2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c0ca3257ff41ef61ae94fa08e4f571f193a63a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebQuanLiKhoaHocApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebQuanLiKhoaHocApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
