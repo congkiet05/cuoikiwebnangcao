749e0418f08754f726be9f1d5a5e0d9b2aa6599c
NguyenLeHuuThuan <huuthuan112000@gmail.com>
2025-12-20 10:04:42 +0700

    sửa lỗi xung đột trong file csproj trong project mvc trong khi merge

--- WebQuanLiKhoaHocApi/WebQuanLiKhoaHocApi/obj/Debug/net8.0/WebQuanLiKhoaHocApi.AssemblyInfo.cs
+++ WebQuanLiKhoaHocApi/WebQuanLiKhoaHocApi/obj/Debug/net8.0/WebQuanLiKhoaHocApi.AssemblyInfo.cs
@@ -15,11 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebQuanLiKhoaHocApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c0ca3257ff41ef61ae94fa08e4f571f193a63a")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937f7606768708443b5b0e86409e04dfab707c64")]
->>>>>>> HuuThuan_XayDungTrangHocVien
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c699a7d836a244fe009c66201601fef34bcd3c2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebQuanLiKhoaHocApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebQuanLiKhoaHocApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
